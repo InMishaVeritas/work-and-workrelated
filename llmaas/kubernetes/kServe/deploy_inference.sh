8325cb95df69d6334621afee380ba1d95fa7189a
Emeric <emeric.renaud@octo.com>
2025-07-16 15:30:02 +0200

    feat(kServe): inference is working with CPU.

--- llmaas/kubernetes/kServe/deploy_inference.sh
+++ llmaas/kubernetes/kServe/deploy_inference.sh
@@ -4,4 +4,4 @@ set -e
 kubectl create namespace kserve-test
 kubectl apply -n kserve-test -f pvc_cache.yaml
 kubectl apply -n kserve-test -f secret.yaml
-kubectl apply -n kserve-test -f inference_test.yaml
+kubectl apply -n kserve-test -f inference_test_cpu.yaml
